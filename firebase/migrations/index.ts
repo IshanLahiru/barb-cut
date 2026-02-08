5f48a739364754f1c1c6654c29c253a4d2443b38
IshanLahiru <ishanlahiru2002@gmail.com>
2026-02-08 12:30:00 +0800

    refactor: Remove old migration files

--- firebase/migrations/index.ts
+++
@@ -1,4 +0,0 @@
-import { migration as migration001 } from "./001_init_users";
-import { migration as migration002 } from "./002_init_styles";
-
-export const MIGRATIONS_REGISTRY = [migration001, migration002];
