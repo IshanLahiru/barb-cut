806a79199457734fc0ef241c203eeee4470bb7ae
IshanLahiru <ishanlahiru2002@gmail.com>
2026-02-18 22:48:24 +0800

    test: update widget test

--- apps/barbcut/ios/test/widget_test.dart
+++ apps/barbcut/ios/test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:barbcut/main.dart';
 
 void main() {
   testWidgets('App launches successfully', (WidgetTester tester) async {
